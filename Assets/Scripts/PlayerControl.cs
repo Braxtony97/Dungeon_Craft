bdf6ab42ae6a687cb4e053063572a2dbe8731354
Renat <renat_samigullin_97@mail.ru>
2023-07-30 23:08:56 +0500

    Added Player movement and Sprite (Skin) for other Players

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,13 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : MonoBehaviour, IPunObservable 
 {
     [SerializeField] private PhotonView _photonView;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite _otherPlayerSprite;
+    private float _speedMove = 5f;
+    private Vector2 _directionLook;
+
+    private void Start()
+    {
+        if (!_photonView.IsMine) _spriteRenderer.sprite = _otherPlayerSprite;
+    }
+
+
     private void Update()
     {
-        if (!_photonView.IsMine) return;
-        if (Input.GetKey(KeyCode.A)) transform.Translate(-Time.deltaTime * 5f, 0f, 0f);
-        if (Input.GetKey(KeyCode.D)) transform.Translate(Time.deltaTime * 5f, 0f, 0f);
+        if (_photonView.IsMine)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                _directionLook = Vector2.left;
+                transform.Translate (Time.deltaTime * _speedMove * _directionLook); 
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _directionLook = Vector2.right;
+                transform.Translate(Time.deltaTime * _speedMove * _directionLook);
+
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                _directionLook = Vector2.up;
+                transform.Translate(Time.deltaTime * _speedMove * _directionLook);
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _directionLook = Vector2.down;
+                transform.Translate(Time.deltaTime * _speedMove * _directionLook);
+
+            }
+        }
+
+        if (_directionLook == Vector2.left) _spriteRenderer.flipX = false;
+        else if (_directionLook == Vector2.right) _spriteRenderer.flipX = true;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting) 
+        {
+            stream.SendNext(_directionLook); 
+        }
+        else if (stream.IsReading) 
+        {
+            _directionLook = (Vector2) stream.ReceiveNext();
+        }
     }
 }
