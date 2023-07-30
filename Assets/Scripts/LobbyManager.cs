e2bb7aac25493689e15d6d755558cc0412f4383d
Renat <renat_samigullin_97@mail.ru>
2023-07-30 18:01:23 +0500

    Add Loading Scene and LoeadingManager script

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -11,19 +11,9 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     [SerializeField] private InputField _nameJoinRoom;
     private void Start()
     {
-        PhotonNetwork.NickName = "Player " + Random.Range(100, 900);
-        Debug.Log("Player's name is set to " + PhotonNetwork.NickName);
+        PhotonNetwork.NickName = "Player " + Random.Range(100, 999);
+        Debug.Log("Player's name: " + PhotonNetwork.NickName);
 
-        PhotonNetwork.AutomaticallySyncScene = true;
-
-        PhotonNetwork.GameVersion = "1";
-
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connected to Master");
     }
 
     public void CreateRoom()
