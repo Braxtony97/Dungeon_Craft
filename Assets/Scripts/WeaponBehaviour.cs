1e646261e148f0d6753e4fc6f8eaf25a3396d8df
Renat <renat_samigullin_97@mail.ru>
2023-08-07 14:54:04 +0500

    Fixed shooting system

--- Assets/Scripts/WeaponBehaviour.cs
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class WeaponBehaviour : MonoBehaviour
 {
     [SerializeField] private float _offset;
+    [SerializeField] private Joystick _joystick;
 
-    private void Update()
+    private void Awake()
     {
+        _joystick = GameObject.FindGameObjectWithTag("JoystickShoot").GetComponent<FixedJoystick>();
+    }
+
+
+    public void FollowingPC ()
+    {
+        _joystick.gameObject.SetActive(false);
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotationZ = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, - (rotationZ + _offset));
+        transform.rotation = Quaternion.Euler(0f, 0f, -(rotationZ + _offset));
+    }
+
+    public void FollowingAndroid()
+    {
+        _joystick.gameObject.SetActive(true);
+        float rotationZ = Mathf.Atan2(_joystick.Horizontal, _joystick.Vertical) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, -(rotationZ + _offset));
+    }
+
+    public bool TouchJoystick()
+    {
+        if (_joystick.Horizontal >= 0.6f || _joystick.Vertical >= 0.6f) return true;
+        else if (_joystick.Horizontal <= -0.6f || _joystick.Vertical <= -0.6f) return true;
+        return false;
     }
 }
